647633757dac0e28fe6df8450569a275835e9d0b
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-01-29 10:02:40 +0530

    take input from user
    created a function to take the users choice, used numbers to denote the 3 choices

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function getComputerChoice(){
     else if(num > 33 && num <= 66)  return( "Paper" );
     else  return( "Scissors" );
 }
+
+function getHumanChoice(){
+    let choice= prompt("Enter:- 1 for 'Rock' \n 2 for 'Scissors' \n3 for 'Paper'");
+
+    if(choice==1) return "Rock";
+    else if(choice == 2) return "Scissors";
+    else return "Paper";
+}
